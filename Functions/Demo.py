dd98c9e8cd7beb3ca0422c4da97085593d3c84ca
balajibala0609 <balajibala062005@gmail.com>
2026-06-03 21:36:56 +0530

    Functions

---
+++ Functions/Demo.py
@@ -0,0 +1,24 @@
+def Balaji(): #function creation
+    print("hai Balaji")
+Balaji() #function calling
+
+#with return
+def Add():
+    cosmo=1005
+    beta=458
+    sum=cosmo+beta
+    return sum
+print("The Adding values",Add())
+
+#without return type
+def sum():
+    gamma=145
+    alpha=755
+    add=gamma+alpha
+    print("Multiply Values",add)
+sum()
+
+#with parameter 
+def name(place):
+    print("cooling place",place)
+name("ooty")
