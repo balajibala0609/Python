08f227f1dd11312c06befcd3c051da08492c8a19
balajibala0609 <balajibala062005@gmail.com>
2026-06-03 21:52:46 +0530

    Functions

---
+++ Functions/Demo2.py
@@ -0,0 +1,11 @@
+def college():
+    college_name=input("Enter the college name")
+    if college_name =="psg":
+        print("better placement in tamilnadu")
+    elif college_name =="kumaraguru":
+        print("high package")
+    elif college_name == "karpagam":
+        print("better college")
+    else:
+        print("not eligible")
+college()
