dea4e99ff82c13ec6ef6e045bc4a83cd726c21f7
balajibala0609 <balajibala062005@gmail.com>
2026-06-11 19:47:12 +0530

    Functions

--- Functions/Parameter1.py
+++ Functions/Parameter1.py
@@ -1,12 +1,23 @@
 #function with default parameter
-def register(name,location,prefix="mr/mrs/miss"):
-    if location == 'bangalore':
-        print(prefix,name,"Has approved in",location)
-    elif location == 'hydrabad':
-        print(prefix,name,'Relocation the hydrabad',location)
-    else:
-        print("Business not approved")
-register('cts','bangalore')
-register('tcs','hydrabad','mr')
-register("finish",'balaji')
+#def register(name,location,prefix="mr/mrs/miss"):
+ #   if location == 'bangalore':
+  #      print(prefix,name,"Has approved in",location)
+   # elif location == 'hydrabad':
+    #    print(prefix,name,'Relocation the hydrabad',location)
+    #else:
+     #   print("Business not approved")
+#register('cts','bangalore')
+#register('tcs','hydrabad','mr')
+#register("finish",'balaji')
 
+#banking
+amount=[15000,25000,45000,3000]
+def debit(money=0,pos=0):
+    if money <= amount[pos]:
+        amount[pos]-=money
+        print(money,'amount')
+        return amount[pos]
+    else:
+        print("cannot debit")
+bank=debit(1500,1)
+debit(bank)
