1d6e04cb0a45b84a5f3ac5a1c08bbb7e6881be9d
balajibala0609 <balajibala062005@gmail.com>
2026-06-17 00:13:42 +0530

    Functions

---
+++ Functions/demolist.py
@@ -0,0 +1,34 @@
+#prop=['Balaji',21,5.4,'M']
+#print(prop)
+
+#adding new element
+#prop.append("kiot")
+#print(prop)
+#print(len(prop))
+
+#adding new element but particullar position
+#prop.insert(3,"BE CSE")
+#print(prop)
+
+#replace element
+#prop[5]='Knowledge college'
+#print(prop)
+
+salary=[60000,70000,80000,85000,90000,105000]
+#print(min(salary))
+#print(max(salary))
+#print(sum(salary))
+
+#remove or pop 
+#salary.remove(60000)
+#print(salary)
+#print(salary.pop(2))
+#print(salary)
+
+#list methods
+#copy and count
+li=[15,45,10,25,30,30]
+alpha=li.copy()
+print("copy data",alpha)
+counting=li.count(30)
+print(counting)
