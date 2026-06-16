8751ebca5b3a64b469912486e445536915f846e0
balajibala0609 <balajibala062005@gmail.com>
2026-06-17 00:33:48 +0530

    Functions

---
+++ Functions/demolist2.py
@@ -0,0 +1,17 @@
+#li=[20,25,30,35,30]
+#alpha=li.copy()
+#print('copy data',alpha)
+#counting=li.count(35)
+#print(counting)
+
+mark=[50,60,70,80,90,100]
+print(min(mark))
+print(max(mark))
+print(sum(mark))
+#remove or pop
+mark.remove(70)
+print(mark)
+print(mark.pop())
+print(mark.pop(2))
+print(mark)
+
