6f2ece085499ddce2a607cc17d52450a3d77fd6e
balajibala0609 <balajibala062005@gmail.com>
2026-06-18 00:17:46 +0530

    Functions

---
+++ Functions/looplist.py
@@ -0,0 +1,22 @@
+#li=input("Enter the list")
+#list=li.split(',')
+#print("list values")
+#for i in list:
+#    print(i)
+
+#looping list
+#n=int(input("enter the list"))
+#Empty_list=[]
+
+#for i in range(n):
+  #  livalue=input("enter value of the list")
+ #   Empty_list.append(livalue)
+#print("list data",Empty_list)
+
+n=int(input("enter the list"))
+empty_list=[]
+for i in range(n):
+    livalue=input("enter value of the list")
+    empty_list.append(livalue)
+print("list data",empty_list)
+    
